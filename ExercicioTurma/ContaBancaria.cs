2d2635d254619d7ba90f878d19cd5492918ce36e
Wanderson Andrade <wmandrade81@gmail.com>
2022-04-15 11:03:01 -0300

    Exercicio Banco Turma Cast

---
+++ ExercicioTurma/ContaBancaria.cs
@@ -0,0 +1,45 @@
+﻿
+
+namespace ExercicioTurma
+{
+    internal class ContaBancaria
+    {
+        public int NumeroDaConta { get; }
+        public string Titular { get; private set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numeroDaConta, string titular, double saldo)
+        {
+            NumeroDaConta = numeroDaConta;
+            Titular = titular;
+            Saldo = saldo;
+        }
+
+        public ContaBancaria(int numeroDaConta, string titular)
+        {
+            NumeroDaConta = numeroDaConta;
+            Titular = titular;
+           
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo -= (valor + 5.00);
+        }
+
+        public void AlteracaoNomeTitular(string nome)
+        {
+            Titular = nome;
+        }
+
+        public override string ToString()
+        {
+            return "\nTitular: " + Titular +  " Conta Número: " + NumeroDaConta + " Saldo de: R$ " + Saldo.ToString("F2") + "\n";
+        }
+    }
+}
---
+++ ExercicioTurma/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ExercicioTurma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+            Console.WriteLine("Bem vindo ao Banco Cast, vamos realizar seu cadastro!");
+            Console.Write("Digite Seu Nome: ");
+            string nome = Console.ReadLine();
+            int numConta = new Random().Next(1000, 9999);
+            Console.WriteLine("Seu número de conta será: " + numConta);
+            Console.Write("Você deseja fazer um saque inicial (s/n) ? ");
+            char resposta = char.Parse(Console.ReadLine().ToLower());
+            if(resposta == 's')
+            {
+                Console.Write("Digite o valor do deposito: ");
+                double deposito = double.Parse(Console.ReadLine());
+                conta = new ContaBancaria(numConta, nome, deposito);
+
+            }
+            else
+            {
+                conta = new ContaBancaria(numConta, nome);
+            }
+            Console.WriteLine(conta);
+            Console.Write("Realizar Deposito !\nDigite o Valor: ");
+            double valor = double.Parse(Console.ReadLine());
+            conta.Deposito(valor);
+            Console.WriteLine("\nDados Atualizados ! \n " + conta);
+            Console.Write("Realizar Saque !\nDigite o Valor: ");
+            valor = double.Parse(Console.ReadLine());
+            conta.Saque(valor);
+            Console.WriteLine("\nDados Atualizados ! \n " + conta);
+        }
+    }
+}
